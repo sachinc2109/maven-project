ed4063445e46217181b75d0bf5073bce6c1ea3e9
sachinc2109 <136722045+sachinc2109@users.noreply.github.com>
2023-06-22 08:06:15 +0530

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+    String someone = "India";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
